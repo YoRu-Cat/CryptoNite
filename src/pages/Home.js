f59f0a259be263bacc835532b4baaa44cbc53579
YoRu Tech <tech.niko.coding@gmail.com>
2024-07-09 13:05:28 +0500

    crypto section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
+import { CryptoProvider } from "../context/CryptoContext";
 
 const Home = () => {
   return (
-    <main
-      className="w-full h-full flex flex-col first-letter:
+    <CryptoProvider>
+      <main
+        className="w-full h-full flex flex-col first-letter:
     content-center items-center relative text-white font-nunito
     "
-    >
-      <div className="w-screen h-screen bg-gray-300 fixed -z-20" />
-      <Logo />
-      <Navigation />
-      <Outlet />
-    </main>
+      >
+        <div className="w-screen h-screen bg-gray-300 fixed -z-20" />
+        <Logo />
+        <Navigation />
+        <Outlet />
+      </main>
+    </CryptoProvider>
   );
 };
 
