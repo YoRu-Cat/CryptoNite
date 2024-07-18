e254b0c4192d69efc5675b6898bc497736a72198
YoRu Tech <tech.niko.coding@gmail.com>
2024-07-18 15:23:16 +0500

    trending page complete with details popup

--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const TrendingCoin = ({ data }) => {
+  let navigate = useNavigate();
+  const getCoinDetails = (id) => {
+    navigate(id);
+  };
   return (
     <div
       className="w-[40%] bg-gray-200 mb-20 
   last:mb-0 rounded-lg p-4 relative cursor-pointer
   hover:bg-gray-300 hover:bg-opacity-40
   "
+      onClick={() => getCoinDetails(data.id)}
     >
       {data ? (
         <>
